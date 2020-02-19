3a12b4950aae207d5e9508137ed382ad27d210ce
shenghui <542594153@qq.com>
2020-02-19 11:44:03 +0800

    nm219

---
+++ nm-service-manage-cms-client/src/main/java/ManageCmsClientApplication.java
@@ -0,0 +1,19 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Created by 赵晟晖 on 2020/2/19.
+ */
+
+@SpringBootApplication
+@EntityScan("com.ningmeng.framework.domain.cms")//扫描实体类
+@ComponentScan(basePackages={"com.ningmeng.framework"})//扫描common下的所有类
+@ComponentScan(basePackages={"com.ningmeng.manage_cms_client"})
+public class ManageCmsClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ManageCmsClientApplication.class, args);
+    }
+
+}
---
+++ nm-service-manage-cms-client/src/main/java/com/ningmeng/manage_cms_client/service/CmsPageService.java
@@ -0,0 +1,13 @@
+package com.ningmeng.manage_cms_client.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by 赵晟晖 on 2020/2/19.
+ */
+@Service
+public class CmsPageService {
+    public void TestRabbit(String id){
+        System.out.println("我被执行到了");
+    }
+}
